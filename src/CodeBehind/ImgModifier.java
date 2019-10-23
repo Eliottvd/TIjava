85117f91c2dd888dfd8a54374b3a81be5f91e9cf
DaniLecx <daniel-garcia-lecloux@hotmail.com>
2019-10-23 10:44:01 +0200

    Added Gaussian filter

--- src/CodeBehind/ImgModifier.java
+++ src/CodeBehind/ImgModifier.java
@@ -297,6 +297,44 @@ public class ImgModifier {
         return ImgOut;
     }
     
+    public BufferedImage getImage(BufferedImage srcImg) {
+
+        BufferedImage finalImg = new BufferedImage(srcImg.getWidth(), srcImg.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Color[] pixel = new Color[9];
+        int[] filter = {1, 2, 1, 
+                        2, 4, 2, 
+                        1, 2, 1};
+        
+        int R, G, B, sum = 0;
+        
+        for(int i=0; i<filter.length; i++)
+            sum += filter[i];
+        
+        for (int i = 1; i < srcImg.getWidth() - 1; i++) {
+            for (int j = 1; j < srcImg.getHeight() - 1; j++) {
+                R = 0;
+                B = 0;
+                G = 0;
+                pixel[0] = new Color(srcImg.getRGB(i - 1, j - 1));
+                pixel[1] = new Color(srcImg.getRGB(i - 1, j));
+                pixel[2] = new Color(srcImg.getRGB(i - 1, j + 1));
+                pixel[3] = new Color(srcImg.getRGB(i, j + 1));
+                pixel[4] = new Color(srcImg.getRGB(i + 1, j + 1));
+                pixel[5] = new Color(srcImg.getRGB(i + 1, j));
+                pixel[6] = new Color(srcImg.getRGB(i + 1, j - 1));
+                pixel[7] = new Color(srcImg.getRGB(i, j - 1));
+                pixel[8] = new Color(srcImg.getRGB(i, j));
+                for (int k = 0; k < 9; k++) {
+                    R += pixel[k].getRed() * filter[k];
+                    B += pixel[k].getBlue() * filter[k];
+                    G += pixel[k].getGreen() * filter[k];
+                }
+                finalImg.setRGB(i, j, new Color(R / sum, G / sum, B / sum).getRGB());
+            }
+        }
+        return finalImg;
+    }
+    
     public BufferedImage FiltreDeKirsh(BufferedImage ImgIn)
     {
         BufferedImage ImgOut = copyImage(ImgIn);
